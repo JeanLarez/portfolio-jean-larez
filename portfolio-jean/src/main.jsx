215dfb466625f74bcbe69999dc2ef167f81e68ab
JeanLarez <jeanlarez@gmail.com>
2023-03-17 23:46:54 -0300

    Projects-Se Agrega componente para presentar tarjetas con enlaces a proyectos

--- portfolio-jean/src/main.jsx
+++ portfolio-jean/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './Components/Header'
+import Grid from './Components/Projects.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Grid />
   </React.StrictMode>,
 )
