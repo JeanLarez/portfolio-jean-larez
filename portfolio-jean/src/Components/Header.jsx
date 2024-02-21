ec42b75cb8300e995014b490b9e8e3599f312289
JeanLarez <jeanlarez@gmail.com>
2024-02-21 08:44:45 -0300

    Cambio imagen del Header

--- portfolio-jean/src/Components/Header.jsx
+++ portfolio-jean/src/Components/Header.jsx
@@ -29,10 +29,10 @@ const Header = () => {
   return (
     <header className="bg-gradient-to-b from-sky-700 via-sky-600 to-sky-500 w-full h-full bg-no-repeat bg-cover bg-center bg-fixed">
       <div className="dark:bg-gray-900 content pt-1.5 px-8">
-        <nav className="flex items-center justify-between">
-          <h2 className="text-white font-bold text-2xl ml-12">Jean Lárez</h2>
+        <nav className="flex items-center justify-between py-2">
+          <h2 className="text-white font-bold text-1xl">Jean Lárez</h2>
           <div className="auth flex items-center mr-12 relative">
-            <button className="text-2xl dark:bg-gray-700 dark:hover:bg-white/40 dark:focus:bg-white/40 bg-gray-800 hover:bg-white/40 w-14 mr-4 px-4 py-2 font-bold text-gray-300 rounded-md peer focus:bg-white/40 focus:text-gray-200 transition-all duration-200">
+            <button className="text-1xl dark:bg-gray-700 dark:hover:bg-white/40 dark:focus:bg-white/40 bg-gray-800 hover:bg-white/40 w-10 mr-2 px-3 py-2 font-bold text-gray-300 rounded-md peer focus:bg-white/40 focus:text-gray-200 transition-all duration-200">
               {darkMode === "dark" ? (
                 <MdOutlineDarkMode />
               ) : (
@@ -40,20 +40,20 @@ const Header = () => {
               )}
             </button>
             <div
-              className='w-32 absolute top-12 z-10
+              className='w-24 absolute top-10 right-12 z-10
 		after:content-[""] after:inline-block after:absolute after:top-0 after:bg-white/40
 		after:w-full after:h-full after:-z-20 after:rounded-lg peer-focus:opacity-100 peer-focus:visible transition-all duration-300 invisible opacity-0 
     '
             >
-              <ul className="py-4 px-3 flex flex-col gap-3">
+              <ul className="py-3 px-3 flex flex-col gap-2">
                 <li
-                  className="flex justify-around items-center cursor-pointer bg-sky-600 p-1 rounded-md hover:opacity-90 text-white text-xl"
+                  className="flex justify-around items-center cursor-pointer bg-sky-600 p-1 rounded-md hover:opacity-90 text-white text-md"
                   onClick={handleClickRemove}
                 >
                   <MdOutlineLightMode /> Light
                 </li>
                 <li
-                  className="flex justify-around items-center  cursor-pointer bg-gray-900 p-1 rounded-md hover:opacity-90 text-white text-xl"
+                  className="flex justify-around items-center  cursor-pointer bg-gray-900 p-1 rounded-md hover:opacity-90 text-white text-md"
                   onClick={handleClickAdd}
                 >
                   <MdOutlineDarkMode /> Dark
@@ -61,24 +61,24 @@ const Header = () => {
               </ul>
             </div>
             <div>
-              <button className=" text-md dark:bg-gray-700 dark:hover:bg-white/40 dark:focus:bg-white/40 bg-gray-800 hover:bg-white/40 w-14 px-3 py-2 font-bold text-gray-300 rounded-md peer focus:bg-white/40 focus:text-gray-200 transition-all duration-200  ">
+              <button className=" text-xs dark:bg-gray-700 dark:hover:bg-white/40 dark:focus:bg-white/40 bg-gray-800 hover:bg-white/40 w-10 px-2 py-2 font-bold text-gray-300 rounded-md peer focus:bg-white/40 focus:text-gray-200 transition-all duration-200  ">
                 {lang}
               </button>
               <div
-                className='w-32 absolute top-12 z-10
+                className='w-24 absolute top-10 z-10
                 after:content-[""] after:inline-block after:absolute after:top-0 after:bg-white/40
                 after:w-full after:h-full after:-z-20 after:rounded-lg peer-focus:opacity-100 peer-focus:visible transition-all duration-300 invisible opacity-0
     '
               >
-                <ul className="py-4 px-3 flex flex-col gap-3">
+                <ul className="py-3 px-3 flex flex-col gap-2">
                   <li
-                    className="flex justify-around items-center cursor-pointer bg-sky-600 p-1 rounded-md hover:opacity-90 text-white text-xl"
+                    className="flex justify-around items-center cursor-pointer bg-sky-600 p-1 rounded-md hover:opacity-90 text-white text-md"
                     onClick={handleClickLangEng}
                   >
                     ENG
                   </li>
                   <li
-                    className="flex justify-around items-center  cursor-pointer bg-gray-900 p-1 rounded-md hover:opacity-90 text-white text-xl"
+                    className="flex justify-around items-center  cursor-pointer bg-gray-900 p-1 rounded-md hover:opacity-90 text-white text-md"
                     onClick={handleClickLangEsp}
                   >
                     ESP
@@ -88,32 +88,32 @@ const Header = () => {
             </div>
           </div>
         </nav>
-        <div className="body mx-8">
+        <div className="body mx-64">
           <div className="flex items-center justify-center">
-            <div className="w-full md:w-1/2 pt-6">
-              <h1 className="text-4xl mb-8 ml-4 font-bold text-white tracking-wide">
+            <div className="w-full md:w-1/2">
+              <h1 className="text-3xl mb-4 ml-4 font-bold text-white tracking-wide">
                 Full Stack Developer
               </h1>
-              <p className="text-gray-100 dark:text-gray-400 text-lg mr-16 mb-8 ml-4">
+              <p className="text-gray-100 dark:text-gray-400 mb-4 ml-4">
                 {lang === "ENG"
-                  ? "I am passionate about developing websites, applications and programs, giving them through industry-standard technologies, structure, appearance and interactive behaviors."
-                  : "Me apasiona desarrollar sitios web, aplicaciones y programas, dándoles mediante tecnologías estándares de la industria, estructura, apariencia y comportamientos interactivos."}
+                  ? "I am passionate about developing websites, applications and programs, giving structure, appearance and interactive behaviors."
+                  : "Me apasiona desarrollar sitios web, aplicaciones y programas, dándoles, estructura, apariencia y comportamientos interactivos."}
               </p>
 
-              <p className="text-gray-100 dark:text-gray-400 text-lg mr-16 mb-8 ml-4">
+              <p className="text-gray-100 dark:text-gray-400 mb-6 ml-4">
                 {lang === "ENG"
                   ? "I also have experience in training people and coordinating teams, allowing me to communicate effectively with colleagues, partners, allies and clients."
-                  : "También tengo experiencia en la formación de personas y la coordinación de equipos, lo que me permite comunicarme eficazmente con colegas, socios, aliados y clientes."}
+                  : "Además tengo experiencia en la formación de personas y la coordinación de equipos, lo que me permite comunicarme eficazmente con colegas, socios, aliados y clientes."}
               </p>
 
-              <button className=" dark:bg-gray-800 bg-gray-900 text-gray-300 ml-3 py-2 px-5 rounded dark:hover:bg-gray-700 hover:bg-gray-800 hover:text-gray-100">
+              <button className=" dark:bg-gray-800 bg-gray-900 text-gray-300 ml-3 py-1 px-5 rounded dark:hover:bg-gray-700 hover:bg-gray-800 hover:text-gray-100">
                 Resume
               </button>
             </div>
-            <div className="w-96 md:max-w-md">
+            <div className="w-80 md:max-w-md">
               <img
                 className="h-max"
-                src="src/assets/jean_perfil_izquierdo.png"
+                src="https://i.imgur.com/lYfQh7r.png"
                 alt="Foto de Jean Larez perfil zquierdo"
               />
             </div>
